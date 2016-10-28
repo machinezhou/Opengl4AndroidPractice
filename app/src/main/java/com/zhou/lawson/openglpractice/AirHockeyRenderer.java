6d31ff5caf0b5664af24f6eb58ba01e9734b96bd
Lawson Zhou <machinezhou@gmail.com>
2016-10-28 17:33:37 +0800

    add color

--- app/src/main/java/com/zhou/lawson/openglpractice/AirHockeyRenderer.java
+++ app/src/main/java/com/zhou/lawson/openglpractice/AirHockeyRenderer.java
@@ -12,14 +12,12 @@ import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_LINES;
 import static android.opengl.GLES20.GL_POINTS;
-import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
-import static android.opengl.GLES20.glGetUniformLocation;
-import static android.opengl.GLES20.glUniform4f;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
@@ -30,28 +28,32 @@ import static android.opengl.GLES20.glViewport;
 
 public final class AirHockeyRenderer implements Renderer {
 
-  private static final int BYTES_PER_FLOAT = 4; //Float.SIZE/8
-  private static final String U_COLOR = "u_Color";
   private static final String A_POSITION = "a_Position";
+  private static final String A_COLOR = "a_Color";
   private static final int POSITION_COMPONENT_COUNT = 2;
+  private static final int COLOR_COMPONENT_COUNT = 3;
+  private static final int BYTES_PER_FLOAT = 4;
+  private static final int STRIDE =
+      (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
   private final FloatBuffer vertexData;
   private final Context context;
-  private int program;
-  private int uColorLocation;
-  private int aPositionLocation;
 
-  public AirHockeyRenderer() {
-    // This constructor shouldn't be called -- only kept for showing
-    // evolution of the code in the chapter.
-    context = null;
-    vertexData = null;
-  }
+  private int program;
+  private int aPositionLocation;
+  private int aColorLocation;
 
   public AirHockeyRenderer(Context context) {
     this.context = context;
     float[] tableVerticesWithTriangles = {
-        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0f,
-        0.5f, 0f, 0f, -0.25f, 0f, 0.25f
+        // Order of coordinates: X, Y, R, G, B
+        // Triangle Fan
+        0f, 0f, 1f, 1f, 1f, -0.5f, -0.5f, 0.7f, 0.7f, 0.7f, 0.5f, -0.5f, 0.7f, 0.7f, 0.7f, 0.5f,
+        0.5f, 0.7f, 0.7f, 0.7f, -0.5f, 0.5f, 0.7f, 0.7f, 0.7f, -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+        // Line 1
+        -0.5f, 0f, 1f, 0f, 0f, 0.5f, 0f, 1f, 0f, 0f,
+        // Mallets
+        0f, -0.25f, 0f, 0f, 1f, 0f, 0.25f, 1f, 0f, 0f
     };
     vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
         .order(ByteOrder.nativeOrder())
@@ -70,12 +72,18 @@ public final class AirHockeyRenderer implements Renderer {
     program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
     ShaderHelper.validateProgram(program);
     glUseProgram(program);
-    uColorLocation = glGetUniformLocation(program, U_COLOR);
+    aColorLocation = glGetAttribLocation(program, A_COLOR);
     aPositionLocation = glGetAttribLocation(program, A_POSITION);
+
     vertexData.position(0);
-    glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0,
+    glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE,
         vertexData);
     glEnableVertexAttribArray(aPositionLocation);
+
+    vertexData.position(POSITION_COMPONENT_COUNT);
+    glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE,
+        vertexData);
+    glEnableVertexAttribArray(aColorLocation);
   }
 
   @Override public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -84,17 +92,9 @@ public final class AirHockeyRenderer implements Renderer {
 
   @Override public void onDrawFrame(GL10 gl) {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
-
-    glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
     glDrawArrays(GL_LINES, 6, 2);
-
-    glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
     glDrawArrays(GL_POINTS, 8, 1);
-
-    glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
     glDrawArrays(GL_POINTS, 9, 1);
   }
 }
